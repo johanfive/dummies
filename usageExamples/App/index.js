3caeb9a1fbc8d47901034e8a25cef9dc559d96f5
Johan <jfhoyhoy@gmail.com>
2020-01-05 15:53:23 -0800

    Use basename prop of BrowserRouter + NavLink

--- usageExamples/App/index.js
+++ usageExamples/App/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import Default from './Default';
 import InputExamples from './InputExamples';
 
 const navStyle = { border: '1px solid #f5f5f5', float: 'right', padding: '1rem' };
 const linkStyle = { color: 'hotpink', margin: '0 1rem' };
+const activeStyle = { ...linkStyle, fontWeight: 'bolder' };
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='/dummies'>
       <nav style={navStyle}>
-        <Link to="/" style={linkStyle}>Home</Link>
-        <Link to="/input" style={linkStyle}>Input</Link>
+        <NavLink exact to="/" style={linkStyle} activeStyle={activeStyle}>Home</NavLink>
+        <NavLink to="/input" style={linkStyle} activeStyle={activeStyle}>Input</NavLink>
       </nav>
       <h1>Dummies</h1>
       <h2>React components library</h2>
       <Switch>
         <Route exact path="/" component={Default} />
-        <Route exact path="/input" component={InputExamples} />
+        <Route path="/input" component={InputExamples} />
       </Switch>
     </Router>
   );
